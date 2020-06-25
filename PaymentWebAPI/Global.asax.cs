56bdfc5b338c4b293d4ee9abdc359e8a93c47094
yo2shi <67420682+yo2shi@users.noreply.github.com>
2020-06-25 17:46:08 +0400

    Add files via upload

---
+++ PaymentWebAPI/Global.asax.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace PaymentWebAPI
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            PaymentWebAPI.Logger.DefaultLogger logger = new Logger.DefaultLogger();
+
+            logger.AppendToLog("Run Started on : " + System.DateTime.Now.ToString("dd/MM/yyyy") + "   at : " + System.DateTime.Now.ToString("HH:mm:ss"));
+            logger.AppendToLog("Global.asax loading...");
+
+            AreaRegistration.RegisterAllAreas();
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+        }
+    }
+}
